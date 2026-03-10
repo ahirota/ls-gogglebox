8c3d56c64dd0b8b96ccccf179724604274a1a986
ahirota <avjhirota@gmail.com>
2026-03-10 14:20:58 -0700

    [GB_1] Basic scaffolding for page

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Global Variables
+const API_BASE = 'https://api.tvmaze.com';
+const SCHEDULE_ENDPOINT = 'schedule';
+const COUNTRY_CODE = 'US';
+
+// Run On Document Ready
+$(async function() {
+    await main();
+});
+
+// Main Call
+async function main() {
+    const data = await getScheduleItems();
+    console.log(data);
+}
+
+// Helper Functions
+// Get item data
+async function getScheduleItems() {
+    const tomorrowDate = formattedTomorrowDate();
+    const apiScheduleURL = `${API_BASE}/${SCHEDULE_ENDPOINT}?country=${COUNTRY_CODE}&date=${tomorrowDate}`;
+
+    try {
+        const result = await $.ajax({
+            url: apiScheduleURL,
+            type: 'GET',
+            dataType: 'json'
+        });
+
+        return result.slice(0,24);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Parse item into usable HTML
+function parseScheduleItemToHTML(scheduleItem) {
+    // TO DO
+}
+
+// Format Tomorrow's Date to ISO 8601 for TV Maze API Consumption
+function formattedTomorrowDate() {
+    const date = new Date();
+    date.setDate(date.getDate() + 1);
+    return date.toISOString().slice(0,10);
+}
